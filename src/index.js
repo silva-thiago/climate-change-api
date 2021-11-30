4b341049048b31c83e86528d99b7d531c31865be
Thiago Silva <thiago.silva@imd.ufrn.br>
2021-11-30 15:56:45 -0300

    ✨ Introduce new features
    - Getting a news source

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
 
 const PORT = 8000
 
@@ -8,4 +10,25 @@ app.listen(PORT, () => console.log(`server running on http://localhost:${ PORT }
 
 app.get('/', (req, res) => {
 	res.json('Welcome to the Climate Change News API')
+})
+
+app.get('/news', (req, res) => {
+	axios.get('https://www.theguardian.com/environment/climate-crisis')
+		.then((response) => {
+			const html = response.data
+			const $ = cheerio.load(html)
+			const articles = []
+
+			$('a:contains("climate")', html).each(function () {
+				const title = $(this).text()
+				const url = $(this).attr('href')
+
+				articles.push({
+					title,
+					url,
+				})
+			})
+
+			res.json(articles)
+		}).catch((err) => console.log(err))
 })
